b4e1ef46d3ee18fd4700c5770bc68ba338e08b08
Elton Santos <eltonaxl@hotmail.com>
2024-10-31 23:03:13 -0300

    Trying get deploy

--- src/app/(paginas)/movie/[id]/page.tsx
+++ src/app/(paginas)/movie/[id]/page.tsx
@@ -7,7 +7,7 @@ interface MoviePageProps {
 }
 
 export default async function MoviePage({ params }: MoviePageProps) {
-  const { id } = await params;
+  const { id } = params;
 
   return (
     <div className="w-[90%] flex flex-col gap-16 md:w-3/4 lg:w-1/2 m-auto mt-14">
